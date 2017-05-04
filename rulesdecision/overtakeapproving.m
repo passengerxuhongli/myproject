fe3a92a5ed5d6a4fc0bce2e2903cef034772d086
xuhong li <836287211@qq.com>
2017-05-04 22:00:17 +0800

    overtaking approving denamic boundary

--- rulesdecision/overtakeapproving.m
+++ rulesdecision/overtakeapproving.m
@@ -1,11 +1,11 @@
 %% 运动执行模块 
-function       [newacceleration] = overtakeapproving(subject_vehicle,leadvehicle)
+function       [newacceleration] = overtakeapproving(subject_vehicle,leadvehicle,LYB)
     newspeed = zeros(1,2);
     newacceleration = zeros(1,2);
     %the distance between subject_vehicle and leadvehicle      
    distance = subject_vehicle(1,4)-leadvehicle(1,4);
    
-   if distance<=-6 %超车接近阶段
+   if distance<=-LYB %超车接近阶段
     lateral_distance = abs(subject_vehicle(1,3)-leadvehicle(1,3));   
      if  lateral_distance<0.6
       delta_speed_lateral = -0.0032*distance-0.21;
@@ -15,7 +15,7 @@ function       [newacceleration] = overtakeapproving(subject_vehicle,leadvehicle
       newacceleration = (newspeed-subject_vehicle(1,5:6))./0.12; 
      end     
    else   %超车偏移和横向保持阶段
-       if  distance<=3%0
+       if  distance<=3
        lateral_distance = abs(subject_vehicle(1,3)-leadvehicle(1,3));   
         if lateral_distance<1.5
         delta_speed_lateral = 0.038*distance-0.002;
